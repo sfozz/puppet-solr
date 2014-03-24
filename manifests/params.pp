3c0842bc27dbb93e25377a8d4f293da48cddb86b
Steve Foster <s.p.foster@leeds.ac.uk>
2014-03-24 16:42:40 +0000

    Allow for redhat and debian in the params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,19 @@
 #
 class solr::params {
 
+  case $::osfamily {
+    redhat: {
+      $jdk_pkg   = 'java-1.7.0-openjdk'
+      $jetty_pkg = 'jetty6'
+    }
+    debian: {
+      $jdk_pkg   = 'default-jdk'
+      $jetty_pkg = [ 'jetty', 'libjetty-extra' ]
+    }
+    default: {
+    }
+  }
+
   $jetty_home = '/usr/share/jetty'
   $solr_home = '/usr/share/solr'
   $solr_version = '4.4.0'
